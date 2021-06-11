d1bd0f5de326d9d8b0c5508f6d69edc1a7e81dde
José Duarte Alleuy <jduartea@gmail.com>
2021-06-11 18:44:07 -0400

    Refactor app to application for AWS EB

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import sys
+
+from blueprints.hellotracks import hellotracks
+from blueprints.braze import braze
+
+application = Flask(__name__)
+
+application.register_blueprint(hellotracks, url_prefix="/hellotracks")
+application.register_blueprint(braze, url_prefix="/braze")
+
+
+@application.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    application.run(host="0.0.0.0")
