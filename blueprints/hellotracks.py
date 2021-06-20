a4018ee7aad41792d66da6e2d2635f771ab9f1d2
José Duarte Alleuy <jduartea@gmail.com>
2021-06-19 20:05:53 -0400

    Updated code using Hellotracks Utils package

--- blueprints/hellotracks.py
+++ blueprints/hellotracks.py
@@ -4,6 +4,7 @@ import random
 from datetime import date
 from dotenv import dotenv_values
 from flask import Blueprint, jsonify, Response, request
+from utils.hellotracks import *
 
 hellotracks = Blueprint(name="hellotracks", import_name=__name__)
 config = dotenv_values(".env")
@@ -20,40 +21,23 @@ def test():
     return jsonify(output)
 
 
-@hellotracks.route('/create_job', methods=['POST', 'GET'])
+@hellotracks.route('/create_job', methods=['POST'])
 def create_job():
-    if request.json["data"]["vehicleStateId"] == 4:
+    if request.json["eventName"] == "vehicleStateChanged" and request.json["data"]["vehicleStateId"] == 4:
         lat = random.uniform(51.47690572136774, 51.538636920666406)
         lon = random.uniform(-0.18411568297429584, -0.07517897752823008)
-        today = date.today()
 
-        workers = ["jose.duarte@humanforest.co.uk", "victor.aguayo@humanforest.co.uk",
-                   "agustin.guilisasti@humanforest.co.uk"]
-        worker = random.choices(workers, k=1)[0]
+        job = create_job_object(
+            job_type=0,
+            destination_lat=lat,
+            destination_lng=lon,
+            destination_text="Fuel Level Low!",
+            custom_attributes={
+                "vehicle_id": request.json["data"]["carId"],
+                "hardware_id": request.json["data"]["hardwareId"]
+            }
+        )
 
-        payload = {"auth": {}, "data": {}}
-        payload["auth"]["usr"] = "jose.duarte@humanforest.co.uk"
-        payload["auth"]["key"] = "7EF691EF8C404ACF1AF7EF99B8FFE10E"
-        payload["data"]["jobs"] = []
+        create_jobs(job_list=list(job))
 
-        job = {
-            "type": 0,
-            "destinationLat": lat,
-            "destinationLng": lon,
-            "destinationText": config["TEST"],
-            "custom_vehicle_id": request.json["data"]["carId"],
-            "custom_vehicle_state_id": request.json["data"]["vehicleStateId"],
-            "custom_hardware_id": request.json["data"]["hardwareId"],
-            "worker": worker,
-            "day": int(today.strftime('%Y%m%d'))
-        }
-
-        payload["data"]["jobs"].append(job)
-
-        requests.post(
-            url="https://hellotracks.com/api/createjobs",
-            headers={
-                "Content-Type": "text/plain; charset=utf-8"
-            },
-            data=json.dumps(payload))
         return Response(status=200)
