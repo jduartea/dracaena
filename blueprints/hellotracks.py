f275ea8b77aa8e141af64aea34182e4e871db873
José Duarte Alleuy <jduartea@gmail.com>
2021-06-19 22:30:45 -0400

    Test Hellotracks create_job endpoint

--- blueprints/hellotracks.py
+++ blueprints/hellotracks.py
@@ -23,8 +23,7 @@ def test():
 def create_job():
     event_name = request.json["eventName"]
     data = request.json["data"]
-    if event_name == "backend\\\\models\\\\VehicleStateChange::afterInsert" \
-            and data["from"] != "fuel level low" \
+    if data["from"] != "fuel level low" \
             and data["to"] == "fuel level low":
         vehicle_id = data["vehicleId"]
         lat = random.uniform(51.47690572136774, 51.538636920666406)
